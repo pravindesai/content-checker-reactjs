784ed560c96b4a49971afe60137067e9dab79744
pravin desai <pravindesai100@gmail.com>
2020-11-07 09:16:11 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var Filter = require('bad-words')
+  var custom_filter = new Filter({placeHolder: '*'})
+  custom_filter.addWords('duck','pass')
+
+  function postBtnClk(){
+    
+      var content = document.getElementById("area").value
+      content = custom_filter.clean(content)
+  
+      document.getElementById("post").innerText = content
+    }
+
+  function clearBtnClk(){
+    
+    document.getElementById("post").innerText = ""
+    document.getElementById("area").value = ""
+  }
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         
+          <div className="textArea_div">
+            <textarea id="area" className="textarea" rows="20" cols="50">
+
+            </textarea>
+          </div>
+
+          <div className="components_div">
+            <button id="post_button" className="Button" onClick={postBtnClk}>POST</button>
+            <button id="clear_button" className="Button" onClick={clearBtnClk}>CLEAR</button>
+            
+          </div>
+
+          <div className="post">
+              <p className = "post" id="post"></p>
+          </div>
+
     </div>
+    
   );
 }
 
